12352e7fd8fb5fc1b05c7795813dbcf810392727
brandonamundson <brandon.m.amundson@gmail.com>
2015-03-05 11:07:37 -0700

    Added Remove Function
    I am pretty sure that the remove function works.  Let me know if
    anything needs to be fixed.

--- Brandon.cpp
+++ Brandon.cpp
@@ -1,1 +1,29 @@
 #include "linklist.h"
+//still need to do getMaxFrequency
+
+bool LinkList::remove(string word)
+{
+    node *temp = nullptr;
+    node *prev = nullptr;
+    prev = temp;
+    temp = headptr;
+
+    while (temp != nullptr)
+    {
+        if (word.compare(temp->word) == 0)
+        {
+            prev->next = temp->next;
+            delete temp;
+            return true;
+        }
+        else
+        {
+            prev = temp;
+            temp = temp->next;
+        }
+    }
+    return false;
+
+}
+
+
