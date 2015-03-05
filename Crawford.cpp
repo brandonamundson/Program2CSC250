724d71a21546cfd648e3650da612f00a7549c267
AlexCrawdad <Alexander.crawford@mines.sdsmt.edu>
2015-03-05 03:09:42 -0700

    Added Find Function I think?
    By the description of the function I am pretty sure I've finished
    this... however I feel like it's an exact copy of the Increment
    Frequency function if I'm understanding this correctly :\

--- Crawford.cpp
+++ Crawford.cpp
@@ -1,3 +1,28 @@
-//write find and size functions.  
-#include "linklist.h"
 
+#include "linklist.h"
+//writing find and size
+LinkList::LinkList()
+{
+	headptr = nullptr;
+}
+
+LinkList::~LinkList()
+{
+	//destructor if anything needs to happen
+}
+ bool LinkList::find(string word)
+ {
+	 node *temp = nullptr;
+
+	 while (temp != nullptr)
+	 {
+		 if (word.compare(temp->word) == 0)
+		 {
+			 return true;
+		 }
+		 else
+			 temp = temp->next;
+
+	 }
+	 return false;
+ }
