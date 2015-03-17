d652e9656ca939415fab9eb0b97b26c8d1e5ab5a
Afrokin <pwncicle@gmail.com>
2015-03-17 15:09:58 -0600

    added isempty started insert

--- Cody.cpp
+++ Cody.cpp
@@ -1,1 +1,17 @@
 #include "linklist.h"
+
+
+
+bool LinkList::insert(string word)
+{
+	node *temp = nullptr;
+	node *prev = nullptr;
+
+
+}
+bool LinkList::isEmpty()
+{
+	if (headptr == NULL)
+		return true;
+	return false;
+}
