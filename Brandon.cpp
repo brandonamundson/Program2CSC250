3cecf915985b9276d2dee44a2b4f14755e068412
brandonamundson <brandon.m.amundson@gmail.com>
2015-03-18 22:16:34 -0600

    Added Doxygen to Brandon.cpp
    I have added Doxygen to my code, let me know if I missed anything or if
    I need to fix my descriptions.

--- Brandon.cpp
+++ Brandon.cpp
@@ -1,5 +1,19 @@
 #include "linklist.h"
 
+
+/***************************************************************************//**
+ * @author Brandon Amundson
+ *
+ * @par Description:
+ * This is the remove function of the program.  It is used when needing to
+ * delete a specific word from the list.
+ *
+ * @param[in]        word - the word that is to be removed from the list.
+ *
+ * @returns true - successfully deleted specified word.
+ * @returns false - word could not be found in list and could not be deleted.
+ *
+ ******************************************************************************/
 bool LinkList::remove(string word)
 {
     node *temp = nullptr;
@@ -26,6 +40,18 @@ bool LinkList::remove(string word)
 }
 
 
+/***************************************************************************//**
+ * @author Brandon Amundson
+ *
+ * @par Description:
+ * This is the function that returns the maximum frequency of any word that is 
+ * located in the list.  It is called by the print function to output the
+ * words in order of frequency as they appear in the list.
+ *
+ *
+ * @returns max - the maximum frequency of the words in the list.
+ *
+ ******************************************************************************/
 int LinkList::getMaxFrequency()
 {
     node *curr = headptr;
